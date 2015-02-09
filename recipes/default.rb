912c293e2e5cfbb300cb00b3c11dcc8967d94505
Myroslav Rys <stonevil@gmail.com>
2015-02-10 00:54:23 +0200

    Fix packer-hyperv URL's

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,14 +9,14 @@ directory "#{ENV['APPDATA']}/packer.d/plugins" do
 end
 
 remote_file "#{ENV['APPDATA']}/packer.d/plugins/packer-builder-hyperv-iso.exe" do
-  source "https://github.com/pbolduc/packer-hyperv/blob/dev/bin/packer-builder-hyperv-iso.exe"
+  source "https://raw.githubusercontent.com/pbolduc/packer-hyperv/blob/dev/bin/packer-builder-hyperv-iso.exe"
   action :create_if_missing
 end
 remote_file "#{ENV['APPDATA']}/packer.d/plugins/packer-post-processor-vagrant.exe" do
-  source "https://github.com/pbolduc/packer-hyperv/blob/dev/bin/packer-post-processor-vagrant.exe"
+  source "https://raw.githubusercontent.com/pbolduc/packer-hyperv/blob/dev/bin/packer-post-processor-vagrant.exe"
   action :create_if_missing
 end
 remote_file "#{ENV['APPDATA']}/packer.d/plugins/packer-provisioner-powershell.exe" do
-  source "https://github.com/pbolduc/packer-hyperv/blob/dev/bin/packer-provisioner-powershell.exe"
+  source "https://raw.githubusercontent.com/pbolduc/packer-hyperv/blob/dev/bin/packer-provisioner-powershell.exe"
   action :create_if_missing
 end
